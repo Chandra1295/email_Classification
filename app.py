9a837e2a92f33000bc699e12067f8a09c3a35b56
Chandra1295 <61043407+Chandra1295@users.noreply.github.com>
2025-01-30 21:38:02 +0530

    Updating app

--- app.py
+++ app.py
@@ -66,5 +66,5 @@ def predict():
     return render_template('result.html', prediction=prediction_result)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000,debug=True)
 
